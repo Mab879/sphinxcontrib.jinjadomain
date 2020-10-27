1b2dbbca9b3fa2c2842eac3782911dc65cde359d
Gabriel Becker <ggasparb@redhat.com>
2020-10-27 15:14:02 +0100

    Fix python3 compatibility error.

--- sphinxcontrib/jinjadomain.py
+++ sphinxcontrib/jinjadomain.py
@@ -93,7 +93,7 @@ class JinjaIndex(Index):
                 ]
             )
         content = content.items()
-        content.sort(key=lambda k: k[0])
+        sorted(content)
         return (content, True)
 
 
